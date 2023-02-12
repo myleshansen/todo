bbf773a0fd653cdc07f8535b7075978abf647b61
myleshansen <myleshunter@hotmail.com>
2023-02-11 17:39:09 -0700

    Gather add project form data

--- src/modules/Dom.js
+++ src/modules/Dom.js
@@ -5,6 +5,7 @@ const Dom = (() => {
   const loadPage = () => {
     loadProjects();
     initMenu();
+    initSideMenu();
   };
   const loadProjects = () => {
     const todoList = TodoList();
@@ -22,7 +23,6 @@ const Dom = (() => {
         menu.style.display = "none";
       }
     });
-
     const addBtn = document.querySelector(".add-btn");
     const addForm = document.querySelector(".task-add-menu");
     addBtn.addEventListener("click", () => {
@@ -32,7 +32,9 @@ const Dom = (() => {
         addForm.style.display = "none";
       }
     });
+  };
 
+  const initSideMenu = () => {
     const addProjectBtn = document.querySelector(".add-project-btn");
     const addProjectForm = document.querySelector(".project-add-menu");
     addProjectBtn.addEventListener("click", () => {
@@ -45,6 +47,25 @@ const Dom = (() => {
         addProjectForm.style.display = "none";
       }
     });
+
+    const projectSubmitBtn = document.querySelector(".project-submit-btn");
+    projectSubmitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      addProject();
+    });
+  };
+
+  const addProject = () => {
+    const projectFormData = new FormData(
+      document.querySelector(".project-form-data")
+    );
+    console.log(projectFormData);
+    // const projectList = document.querySelector(".project-list");
+    // const projectItem = document.createElement("div");
+    // projectItem.classList.add("project-item");
+    // projectItem.setAttribute("data-project", project.title);
+    // projectItem.textContent = project.title;
+    // projectList.appendChild(projectItem);
   };
 
   return {
